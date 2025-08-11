b9e0778b05aa14b7041bb12a502c752cd8ff5d14
Maximilian Spoljaric <spoljaricmax@gmail.com>
2025-08-11 15:57:09 +0300

    Add extra credit behavior
    Modified divMaker function to take in the size of
    an single side of the grid, making it easier to
    call it on the buttons event listener that resizes
    the grid.
    
    Added more actions to event listener, these change
    the color of a square to a random rgb color and
    increased the opacity by 0.1 (initial value was
    set to 0.1).

--- javascript.js
+++ javascript.js
@@ -3,13 +3,17 @@ let sizeButton = document.getElementById("sizebutton");
 
 function divMaker(parent, number){
     let size = number*number;
+    
     for (let index = 1; index <= size; index++){
         let div = document.createElement("div");
         div.className ="square";
         div.style.flex = "1 0 "+(100/(number))+"%";
+        
         div.addEventListener("mouseover", (event) => {
-            div.style.backgroundColor = "yellow";
+            div.style.opacity = +div.style.opacity + 0.1;          
+            div.style.backgroundColor = "rgb("+(Math.random()*(255 - 0) + 0)+","+(Math.random()*(255 - 0) + 0)+","+(Math.random()*(255 - 0) + 0)+")";    
         });
+        
         squareContainer.appendChild(div);
       
     }
@@ -19,18 +23,12 @@ divMaker(squareContainer, 16);
 
 sizeButton.addEventListener("click", () => {
    let promptNumber = window.prompt("enter size by amount of squares per side:", "max. 100"); 
+   
+   while(promptNumber>100){
+        promptNumber = window.prompt("nope, too much. under 100 please.");
+   }
+
    squareContainer.innerHTML ="";
    divMaker(squareContainer, promptNumber);
 });
 
-/*
-document.addEventListener("mouseover", (event)=>{
-    let target = event.target;
-    console.log(target.id)
-    if (event.target.id == "square") {
-        event.target.className="hover";
-        
-    }
-
-});
-*/
